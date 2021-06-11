39ccd24a4949c22729bf8b7ef12afb8746a6bc19
hcankurt0766 <hcankurt0766@gmail.com>
2021-06-11 12:36:44 +0300

    ii

--- odev/odev/Database/Model1.Designer.cs
+++ odev/odev/Database/Model1.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\YAZILIM PROJE\projee\odev\odev\Database\Model1.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\iekem\source\repos\odev\odev\Database\Model1.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
